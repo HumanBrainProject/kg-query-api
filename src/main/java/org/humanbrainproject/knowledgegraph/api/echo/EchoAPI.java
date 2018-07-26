03391524f38f27772a7ff96fb47404f097f2e9b5
Oliver Schmid <oliver.schmid@epfl.ch>
2018-07-26 16:20:32 +0200

    apply logging mechanism for integration purposes

--- src/main/java/org/humanbrainproject/knowledgegraph/api/echo/EchoAPI.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/api/echo/EchoAPI.java
@@ -2,17 +2,27 @@ package org.humanbrainproject.knowledgegraph.api.echo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping(value = "/echo")
 public class EchoAPI {
 
-    @GetMapping
+    protected Logger log = Logger.getLogger(EchoAPI.class.getName());
+
     @PostMapping
     @PutMapping
     @DeleteMapping
     @PatchMapping
     public String echo(@RequestBody String request){
+        log.info(String.format("Echoing %s", request));
         return request;
+    }
+
+    @GetMapping
+    public String echo(){
+        log.info("Echoing to GET");
+        return "Hello world";
     }
 
 }
--- src/main/java/org/humanbrainproject/knowledgegraph/api/indexation/ArangoIndexationAPI.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/api/indexation/ArangoIndexationAPI.java
@@ -1,6 +1,7 @@
 package org.humanbrainproject.knowledgegraph.api.indexation;
 
 import org.humanbrainproject.knowledgegraph.boundary.indexation.ArangoIndexation;
+import org.humanbrainproject.knowledgegraph.control.VertexRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(value = "/arango", consumes = {MediaType.APPLICATION_JSON, "application/ld+json"}, produces = MediaType.APPLICATION_JSON)
@@ -16,9 +18,11 @@ public class ArangoIndexationAPI implements KGIndexationAPI {
     @Autowired
     ArangoIndexation indexer;
 
+    protected Logger log = Logger.getLogger(ArangoIndexationAPI.class.getName());
 
     @PostMapping
     public void uploadToPropertyGraph(@RequestBody String payload, @QueryParam("defaultNamespace") String defaultNamespace, @QueryParam("vertexLabel") String vertexLabel) throws IOException, JSONException {
+        log.info(String.format("Processing %s", payload));
         indexer.uploadJsonOrJsonLd(payload, defaultNamespace, vertexLabel);
     }
 
