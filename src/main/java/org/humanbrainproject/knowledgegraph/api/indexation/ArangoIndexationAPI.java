6baba9c89c12cf0547eda660bd43ccf06b12acfe
Oliver Schmid <oliver.schmid@epfl.ch>
2018-07-30 11:34:05 +0200

    wrap json exceptions to bad request responses.

--- src/main/java/org/humanbrainproject/knowledgegraph/api/indexation/ArangoIndexationAPI.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/api/indexation/ArangoIndexationAPI.java
@@ -7,6 +7,7 @@ import org.humanbrainproject.knowledgegraph.boundary.indexation.ArangoIndexation
 import org.humanbrainproject.knowledgegraph.control.VertexRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.PathParam;
@@ -28,24 +29,42 @@ public class ArangoIndexationAPI {
 
     @ApiOperation("Creates a new instance")
     @PostMapping(value="/{organization}/{domain}/{schema}/{schemaversion}/{id}", consumes = {MediaType.APPLICATION_JSON, "application/ld+json"}, produces = MediaType.APPLICATION_JSON)
-    public void addInstance(@RequestBody String payload, @PathVariable("organization") String organization, @PathVariable("domain") String domain, @PathVariable("schema") String schema, @PathVariable("schemaversion") String schemaVersion, @PathVariable("id") String id) throws IOException, JSONException {
+    public ResponseEntity<String> addInstance(@RequestBody String payload, @PathVariable("organization") String organization, @PathVariable("domain") String domain, @PathVariable("schema") String schema, @PathVariable("schemaversion") String schemaVersion, @PathVariable("id") String id) throws IOException {
         String entityName = buildEntityName(organization, domain, schema, schemaVersion);
         log.info(String.format("Received insert request for %s/%s: %s", entityName, id, payload));
-        indexer.insertJsonOrJsonLd(entityName, id, payload, buildDefaultNamespace(organization, domain, schema, schemaVersion));
+        try {
+            indexer.insertJsonOrJsonLd(entityName, id, payload, buildDefaultNamespace(organization, domain, schema, schemaVersion));
+            return ResponseEntity.ok(null);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @PutMapping(value="/{organization}/{domain}/{schema}/{schemaversion}/{id}/{rev}", consumes = {MediaType.APPLICATION_JSON, "application/ld+json"}, produces = MediaType.APPLICATION_JSON)
-    public void updateInstance(@RequestBody String payload, @PathVariable("organization") String organization, @PathVariable("domain") String domain, @PathVariable("schema") String schema, @PathVariable("schemaversion") String schemaVersion, @PathVariable("id") String id, @PathVariable("rev") Integer rev, @QueryParam("defaultNamespace") String defaultNamespace, @QueryParam("vertexLabel") String vertexLabel) throws IOException, JSONException {
+    public ResponseEntity<String> updateInstance(@RequestBody String payload, @PathVariable("organization") String organization, @PathVariable("domain") String domain, @PathVariable("schema") String schema, @PathVariable("schemaversion") String schemaVersion, @PathVariable("id") String id, @PathVariable("rev") Integer rev, @QueryParam("defaultNamespace") String defaultNamespace, @QueryParam("vertexLabel") String vertexLabel) throws IOException {
         String entityName = buildEntityName(organization, domain, schema, schemaVersion);
         log.info(String.format("Received update request for %s/%s in rev %s: %s", entityName, id, rev, payload));
-        indexer.updateJsonOrJsonLd(entityName, id, rev, payload, buildDefaultNamespace(organization, domain, schema, schemaVersion));
+        try {
+            indexer.updateJsonOrJsonLd(entityName, id, rev, payload, buildDefaultNamespace(organization, domain, schema, schemaVersion));
+            return ResponseEntity.ok(null);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @DeleteMapping(value="/{organization}/{domain}/{schema}/{schemaversion}/{id}", produces = MediaType.APPLICATION_JSON)
-    public void deleteInstance(@PathVariable("organization") String organization, @PathVariable("domain") String domain, @PathVariable("schema") String schema, @PathVariable("schemaversion") String schemaVersion, @PathVariable("id") String id, @RequestAttribute(value="rev", required = false) Integer rev) throws IOException, JSONException {
+    public ResponseEntity<String> deleteInstance(@PathVariable("organization") String organization, @PathVariable("domain") String domain, @PathVariable("schema") String schema, @PathVariable("schemaversion") String schemaVersion, @PathVariable("id") String id, @RequestAttribute(value="rev", required = false) Integer rev) throws IOException {
         String entityName = buildEntityName(organization, domain, schema, schemaVersion);
         log.info(String.format("Received delete request for %s/%s in rev %s", entityName, id, rev));
-        indexer.delete(entityName, id, rev);
+        try {
+            indexer.delete(entityName, id, rev);
+            return ResponseEntity.ok(null);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     private String buildEntityName(String organization, String domain, String schema, String schemaVersion){
