80285e9cfcacc7ecf88ea38edad0ca481e3c7d48
Oliver Schmid <oliver.schmid@epfl.ch>
2018-09-03 17:28:44 +0200

    initial commit for spatial search (wip)

--- src/main/java/org/humanbrainproject/knowledgegraph/control/spatialSearch/SpatialSearchController.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/control/spatialSearch/SpatialSearchController.java
@@ -1,41 +1,29 @@
 package org.humanbrainproject.knowledgegraph.control.spatialSearch;
 
+import org.humanbrainproject.knowledgegraph.control.arango.ArangoRepository;
 import org.humanbrainproject.knowledgegraph.control.solr.SolrRepository;
-import org.humanbrainproject.knowledgegraph.entity.jsonld.JsonLdVertex;
+import org.humanbrainproject.knowledgegraph.entity.indexing.SpatialAnchoring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 @Component
 public class SpatialSearchController {
 
+
     @Autowired
     SolrRepository solrRepository;
 
+    @Autowired
+    ArangoRepository arangoRepository;
 
-    public boolean isRelevant(List<JsonLdVertex> jsonLdVertices){
-        return false;
+    public void index(SpatialAnchoring anchoring){
+        if(anchoring!=null) {
+            System.out.println("Indexing "+anchoring);
+        }
     }
 
-    public void remove(Map instance) {
+    public void remove(SpatialAnchoring spatialAnchoring) {
 
-    }
 
-    public boolean isRelevant(Map instance){
-        return false;
-    }
-
-    public void index(List<JsonLdVertex> jsonLdVertices){
-
-    }
-
-    public List<List<String>> findDocumentsToBeRemovedFromSpatialSearch(List<JsonLdVertex> jsonLdVertices){
-        return Collections.emptyList();
-    }
-
-    public void remove(List<List<String>> elementsToBeRemovedFromSpatialSearch) {
     }
 }
