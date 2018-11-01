3bf5ac40d8c44190d9d60a05da306a0f677339d2
Oliver Schmid <oliver.schmid@epfl.ch>
2018-11-01 13:51:15 +0100

    expose schema creation through nexus extension api.

--- src/main/java/org/humanbrainproject/knowledgegraph/nexusExt/api/NexusExtensionAPI.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/nexusExt/api/NexusExtensionAPI.java
@@ -1,15 +1,39 @@
 package org.humanbrainproject.knowledgegraph.nexusExt.api;
 
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.humanbrainproject.knowledgegraph.commons.propertyGraph.entity.SubSpace;
+import org.humanbrainproject.knowledgegraph.indexing.entity.nexus.NexusSchemaReference;
+import org.humanbrainproject.knowledgegraph.nexusExt.boundary.NexusExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
 @RestController
 @RequestMapping(value = "/nexus", produces = MediaType.APPLICATION_JSON)
 @Api(value="/nexus", description = "The extension API for managing resources on Nexus")
 public class NexusExtensionAPI {
+
+    @Autowired
+    NexusExtension nexusExtension;
+
+
+    @PutMapping(value = "/schemas/{org}/{domain}/{schema}/{version}", consumes = { MediaType.APPLICATION_JSON})
+    public ResponseEntity<Void> createSimpleSchema(@PathVariable("org") String org, @PathVariable("domain") String domain, @PathVariable("schema") String schema, @PathVariable("version") String version, @RequestParam(value = "subSpace", required = false) SubSpace subSpace ){
+        try{
+            NexusSchemaReference schemaReference = new NexusSchemaReference(org, domain, schema, version);
+            if(subSpace!=null){
+                schemaReference = schemaReference.toSubSpace(subSpace);
+            }
+            nexusExtension.createSimpleSchema(schemaReference);
+            return ResponseEntity.ok().build();
+        } catch (IOException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
 
 }
--- src/main/java/org/humanbrainproject/knowledgegraph/nexusExt/boundary/NexusExtension.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/nexusExt/boundary/NexusExtension.java
@@ -3,9 +3,11 @@ package org.humanbrainproject.knowledgegraph.nexusExt.boundary;
 import org.humanbrainproject.knowledgegraph.indexing.entity.nexus.NexusSchemaReference;
 import org.humanbrainproject.knowledgegraph.nexusExt.control.SchemaController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class NexusExtension {
 
     @Autowired
