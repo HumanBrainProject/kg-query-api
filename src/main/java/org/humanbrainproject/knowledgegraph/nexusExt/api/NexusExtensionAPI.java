b6b3c54b12875c0573c93cd582c04b9cffedcee6
David Kunzmann <david.kunzmann@epfl.ch>
2018-11-02 08:49:22 +0100

    Added endpoint for schema creation

--- src/main/java/org/humanbrainproject/knowledgegraph/nexusExt/api/NexusExtensionAPI.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/nexusExt/api/NexusExtensionAPI.java
@@ -1,6 +1,7 @@
 package org.humanbrainproject.knowledgegraph.nexusExt.api;
 
 import io.swagger.annotations.Api;
+import org.apache.commons.configuration.ConversionException;
 import org.humanbrainproject.knowledgegraph.commons.propertyGraph.entity.SubSpace;
 import org.humanbrainproject.knowledgegraph.indexing.entity.nexus.NexusSchemaReference;
 import org.humanbrainproject.knowledgegraph.nexusExt.boundary.NexusExtension;
@@ -21,19 +22,29 @@ public class NexusExtensionAPI {
     NexusExtension nexusExtension;
 
 
+
     @PutMapping(value = "/schemas/{org}/{domain}/{schema}/{version}", consumes = { MediaType.APPLICATION_JSON})
-    public ResponseEntity<Void> createSimpleSchema(@PathVariable("org") String org, @PathVariable("domain") String domain, @PathVariable("schema") String schema, @PathVariable("version") String version, @RequestParam(value = "subSpace", required = false) SubSpace subSpace ){
+    public ResponseEntity<Void> createSimpleSchema(@PathVariable("org") String org, @PathVariable("domain") String domain, @PathVariable("schema") String schema, @PathVariable("version") String version, @RequestParam(value = "subSpace", required = false) String subSpace ){
         try{
+
             NexusSchemaReference schemaReference = new NexusSchemaReference(org, domain, schema, version);
             if(subSpace!=null){
-                schemaReference = schemaReference.toSubSpace(subSpace);
+                SubSpace space = SubSpace.byPostfix(subSpace);
+                if(space != null){
+                    schemaReference = schemaReference.toSubSpace(space);
+                }else{
+                    throw new ConversionException("Could not convert subspace");
+                }
             }
             nexusExtension.createSimpleSchema(schemaReference);
             return ResponseEntity.ok().build();
         } catch (IOException e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (ConversionException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
 
 }
+
