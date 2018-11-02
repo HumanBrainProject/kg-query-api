27c7e8d45f90bb6eaaa704298bd6396c5155ce84
Oliver Schmid <oliver.schmid@epfl.ch>
2018-11-02 12:51:39 +0100

    point to *new arango dbs

--- src/main/java/org/humanbrainproject/knowledgegraph/KgQueryApplication.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/KgQueryApplication.java
@@ -36,13 +36,13 @@ public class KgQueryApplication {
 	@Bean
 	@Qualifier("default")
 	public ArangoConnection createDefaultDb() {
-		return new ArangoConnection("kg_oli", false);
+		return new ArangoConnection("kg_new", false);
 	}
 
 	@Bean
 	@Qualifier("inferred")
 	public ArangoConnection createInferredDb() {
-		return new ArangoConnection("kg_oli_inferred", true);
+		return new ArangoConnection("kg_inferred", true);
 	}
 
 	@Bean
@@ -66,6 +66,6 @@ public class KgQueryApplication {
 	@Bean
 	@Qualifier("released")
 	public ArangoConnection createReleasedDb() {
-		return new ArangoConnection("kg_oli_released", false);
+		return new ArangoConnection("kg_released_new", false);
 	}
 }
