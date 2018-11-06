7c8645f196faeff334abc79c3ecbf38f194797d9
Oliver Schmid <oliver.schmid@epfl.ch>
2018-11-06 09:32:43 +0100

    fix tests

--- src/test/java/org/humanbrainproject/knowledgegraph/commons/propertyGraph/arango/control/ArangoRepositoryTest.java
+++ src/test/java/org/humanbrainproject/knowledgegraph/commons/propertyGraph/arango/control/ArangoRepositoryTest.java
@@ -24,11 +24,32 @@ public class ArangoRepositoryTest {
 
         ReleaseStatus worstReleaseStatus = new ArangoRepository().findWorstReleaseStatusOfChildren(map, null, true);
 
-        Assert.assertEquals(ReleaseStatus.HAS_CHANGED, worstReleaseStatus);
+        //The worst release state is RELEASED, because root is not taken into account
+        Assert.assertEquals(ReleaseStatus.RELEASED, worstReleaseStatus);
     }
 
     @Test
     public void findWorstReleaseStatus2() {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("status", ReleaseStatus.HAS_CHANGED.name());
+        List<Object> children = new ArrayList<>();
+        Map<String, Object> childMap = new HashMap<>();
+        childMap.put("status", ReleaseStatus.RELEASED.name());
+        children.add(childMap);
+        Map<String, Object> childMap2 = new HashMap<>();
+        childMap2.put("status", ReleaseStatus.HAS_CHANGED.name());
+        children.add(childMap2);
+        map.put("children", children);
+
+        ReleaseStatus worstReleaseStatus = new ArangoRepository().findWorstReleaseStatusOfChildren(map, null, true);
+
+        //The worst release state is RELEASED, because root is not taken into account
+        Assert.assertEquals(ReleaseStatus.HAS_CHANGED, worstReleaseStatus);
+    }
+
+    @Test
+    public void findWorstReleaseStatus3() {
 
         Map<String, Object> map = new HashMap<>();
         map.put("status", ReleaseStatus.HAS_CHANGED.name());
