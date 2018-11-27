efcff83fa2303b60c4656b5f9326cad93027cc36
Oliver Schmid <oliver.schmid@epfl.ch>
2018-11-27 11:19:47 +0100

    move releasing instances to its own space

--- src/main/java/org/humanbrainproject/knowledgegraph/instances/control/NexusReleasingController.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/instances/control/NexusReleasingController.java
@@ -25,6 +25,9 @@ public class NexusReleasingController {
     InstanceController instanceController;
 
     @Autowired
+    SchemaController schemaController;
+
+    @Autowired
     NexusConfiguration configuration;
 
     @Autowired
@@ -38,7 +41,7 @@ public class NexusReleasingController {
         payload.addReference(HBPVocabulary.RELEASE_INSTANCE, configuration.getAbsoluteUrl(instanceReference));
         payload.put(HBPVocabulary.RELEASE_REVISION, revision);
         payload.addType(HBPVocabulary.RELEASE_TYPE);
-        NexusSchemaReference releaseSchema = new NexusSchemaReference(instanceReference.getNexusSchema().getOrganization(), "prov", "release", "v0.0.2");
+        NexusSchemaReference releaseSchema = new NexusSchemaReference("releasing", "prov", "release", "v0.0.2");
         NexusInstanceReference instance = instanceController.createInstanceByIdentifier(releaseSchema, instanceReference.getFullId(false), payload, credential);
         return new IndexingMessage(instance, jsonTransformer.getMapAsJson(payload), null, null);
     }
