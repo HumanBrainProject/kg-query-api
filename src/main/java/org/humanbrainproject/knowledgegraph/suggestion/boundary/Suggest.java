708e28b85131c610250f530f957a62af3367e26d
Oliver Schmid <oliver.schmid@epfl.ch>
2019-01-21 15:06:13 +0100

    add values to suggestion which haven't been selected yet but are instances of already used schemas.

--- src/main/java/org/humanbrainproject/knowledgegraph/suggestion/boundary/Suggest.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/suggestion/boundary/Suggest.java
@@ -2,12 +2,13 @@ package org.humanbrainproject.knowledgegraph.suggestion.boundary;
 
 import org.humanbrainproject.knowledgegraph.indexing.entity.nexus.NexusSchemaReference;
 import org.humanbrainproject.knowledgegraph.query.entity.Pagination;
+import org.humanbrainproject.knowledgegraph.query.entity.QueryResult;
 import org.humanbrainproject.knowledgegraph.suggestion.control.SuggestionController;
-import org.humanbrainproject.knowledgegraph.suggestion.entity.Suggestion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class Suggest {
@@ -15,7 +16,7 @@ public class Suggest {
     @Autowired
     SuggestionController suggestionController;
 
-    public List<Suggestion> suggestByField(NexusSchemaReference schemaReference, String field, String search, Pagination pagination){
+    public QueryResult<List<Map>> suggestByField(NexusSchemaReference schemaReference, String field, String search, Pagination pagination){
         return suggestionController.simpleSuggestByField(schemaReference, field, search, pagination);
     }
 
