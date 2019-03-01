3f0df2bc075edcdd723bd368d96bc65f5984bb61
David Kunzmann <davidkunzmann@gmail.com>
2019-03-01 15:04:06 +0100

    Corrected reconciliation tests

--- src/main/java/org/humanbrainproject/knowledgegraph/indexing/entity/Alternative.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/indexing/entity/Alternative.java
@@ -17,6 +17,9 @@ public class Alternative extends LinkedHashMap<String, Object> {
     public Object getValue() {
         return this.get(HBPVocabulary.INFERENCE_ALTERNATIVES_VALUE);
     }
+    public Boolean getIsSelected() {
+        return (Boolean) this.get(HBPVocabulary.INFERENCE_ALTERNATIVES_SELECTED);
+    }
 
     public Set<String> getUserIds() {
         return (Set<String>)this.get(HBPVocabulary.INFERENCE_ALTERNATIVES_USERIDS);
