daed585c5f3bea5cb264b09547e874183bfa5478
Oliver Schmid <oliver.schmid@epfl.ch>
2019-05-02 16:05:54 +0200

    #53 for unreleasing, we need to find the releases in inferred space but against the original id in main space...

--- src/main/java/org/humanbrainproject/knowledgegraph/releasing/control/ReleaseControl.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/releasing/control/ReleaseControl.java
@@ -237,7 +237,7 @@ public class ReleaseControl {
                 NexusInstanceReference fromUrl = NexusInstanceReference.createFromUrl((String) relativeUrl);
                 NexusInstanceReference originalFrom = nativeRepository.findOriginalId(fromUrl);
                 //Find release instance
-                Set<NexusInstanceReference> releases = nativeRepository.findOriginalIdsWithLinkTo(databaseFactory.getDefaultDB(), ArangoDocumentReference.fromNexusInstance(originalFrom), ArangoCollectionReference.fromFieldName(HBPVocabulary.RELEASE_INSTANCE));
+                Set<NexusInstanceReference> releases = nativeRepository.findOriginalIdsWithLinkTo(databaseFactory.getInferredDB(), ArangoDocumentReference.fromNexusInstance(originalFrom.toSubSpace(SubSpace.MAIN)), ArangoCollectionReference.fromFieldName(HBPVocabulary.RELEASE_INSTANCE));
                 for (NexusInstanceReference nexusInstanceReference : releases) {
                     Integer currentRevision = nativeRepository.getCurrentRevision(ArangoDocumentReference.fromNexusInstance(nexusInstanceReference));
                     nexusInstanceReference.setRevision(currentRevision);
