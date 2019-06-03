7136259e2f9b8cbec50af95e9599f90a5b56c7d7
Oliver Schmid <oliver.schmid@epfl.ch>
2019-06-03 14:00:58 +0200

    #161: Wrong index mapping -> revert to simple structure

--- src/main/java/org/humanbrainproject/knowledgegraph/commons/solr/Solr.java
+++ src/main/java/org/humanbrainproject/knowledgegraph/commons/solr/Solr.java
@@ -8,7 +8,6 @@ import org.apache.solr.client.solrj.request.CoreAdminRequest;
 import org.apache.solr.client.solrj.request.schema.FieldTypeDefinition;
 import org.apache.solr.client.solrj.request.schema.SchemaRequest;
 import org.apache.solr.client.solrj.response.Group;
-import org.apache.solr.client.solrj.response.GroupCommand;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.apache.solr.common.SolrInputDocument;
@@ -209,8 +208,7 @@ public class Solr implements InitializingBean {
         query.setRows(Long.valueOf(matches).intValue());
         query.setParam("group", true);
         query.setParam("group.field", "aid");
-        List<Group> groups = query(query).getGroupResponse().getValues().get(0).getValues();
-        return groups.stream().map(group -> (String)((List)group.getResult().get(0).get("aid")).get(0)).collect(Collectors.toList());
+        return query(query).getGroupResponse().getValues().get(0).getValues().stream().map(Group::getGroupValue).collect(Collectors.toList());
     }
 
 
