b9d8f46f67266d46f2f006d6d936698d63d72e87
Oliver Schmid <oliver.schmid@epfl.ch>
2021-01-08 14:51:30 +0100

    fix url for python generated queries

--- src/main/resources/codegenerator/python/queryApi.py
+++ src/main/resources/codegenerator/python/queryApi.py
@@ -30,7 +30,7 @@ class KGClient:
         self.http_client = HttpClient(endpoint, "", auth_client=auth_client)
 
     def query(self, root_schema, query_name, size, start, filter_parameters):
-        url = "{}/{}/instances?databaseScope={}&size={}&start={}{}".format("RELEASED" if self.released else "INFERRED", root_schema, query_name, size if size is not None else "", start if start is not None else "", filter_parameters if filter_parameters is not None else "")
+        url = "{}/{}/instances?databaseScope={}&size={}&start={}{}".format(root_schema, query_name, "RELEASED" if self.released else "INFERRED", size if size is not None else "", start if start is not None else "", filter_parameters if filter_parameters is not None else "")
         return self.http_client.get(url)
 
     def released(self):
